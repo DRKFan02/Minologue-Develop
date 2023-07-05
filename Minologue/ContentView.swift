34724f04d4d7ebbf9680fd2df3dab454dea90f7e
DerekFan7 <derekfan@usc.edu>
2023-07-05 08:55:54 +0800

    Initial Commit

---
+++ Minologue/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Minologue
+//
+//  Created by 范家齐 on 7/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Minologue/MinologueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinologueApp.swift
+//  Minologue
+//
+//  Created by 范家齐 on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MinologueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
